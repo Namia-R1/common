27ec75f1502c7c11c99c5599b5ba294f35bd8cac
Namia-R1 <mk258855@163.com>
2024-11-30 00:35:03 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -322,6 +322,7 @@ cat >>"feeds.conf.default" <<-EOF
 src-git danshui1 https://github.com/Namia-R1/openwrt-package.git;${SOURCE}
 src-git helloworld https://github.com/fw876/helloworld.git
 src-git passwall3 https://github.com/xiaorouji/openwrt-passwall-packages;main
+src-git 5gmodem https://github.com/Namia-R1/5G-Modem-Support;main
 EOF
 ./scripts/feeds update -a
 
